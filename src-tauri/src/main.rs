19f7033581bd1f470c3004b9894ed18b1437a3a8
Rewant <rewantgoenka87076@gmail.com>
2026-03-01 20:57:25 +0530

    linuxfix

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
-    // Fix for EGL/Webview issues on certain Linux distros
+    // Set this BEFORE initializing the Tauri app
     std::env::set_var("WEBKIT_DISABLE_COMPOSITING_MODE", "1");
-    anchor_lib::run()
+    
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
